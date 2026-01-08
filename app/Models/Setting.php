bb4baa8a2511060de363703ac86dd2ebabf8ff99
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-09 07:32:22 +0800

    Update Setting.php

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    //
+    protected $fillable = ['key', 'value', 'description'];
+
+    /**
+     * Get a setting value by key
+     */
+    public static function get(string $key, $default = null)
+    {
+        $setting = self::where('key', $key)->first();
+        return $setting ? $setting->value : $default;
+    }
+
+    /**
+     * Set a setting value
+     */
+    public static function set(string $key, $value, ?string $description = null)
+    {
+        return self::updateOrCreate(
+            ['key' => $key],
+            ['value' => $value, 'description' => $description]
+        );
+    }
 }
