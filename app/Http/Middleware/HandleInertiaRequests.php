76ecf3d0a0374415d7a95a920a24dad95c814990
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-24 13:57:14 +0800

    Update HandleInertiaRequests.php

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -41,6 +41,7 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'name' => config('app.name'),
+            'event_name' => \App\Models\Setting::get('event_name', 'EVENTTITLE'),
             'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth' => [
                 'user' => $request->user(),
